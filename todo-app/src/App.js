f2ce4106875fd63986dce14c8ec734ff6c65dcab
Shubham05116 <shubham05116@gmail.com>
2023-09-10 01:37:53 +0530

    commit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,19 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './components/Form';
-import Tasks from './components/Tasks';
-import Header from './components/Header';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [lists, setList] = useState([]);
+  const [input, setInput] = useState('');
+  const [editChange, setEditChange] = useState('');
+  const [editItemId, setEditItemId] = useState(null);
+
+  function changeHandler(e) {
+    setInput(e.target.value);
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    setList((prev) => [
+      ...prev,
+      { text: input, id: Math.random().toString() },
+    ]);
+    setInput('');
+  }
+
+  function DeleteHandler(id) {
+    const newData = lists.filter((list) => list.id !== id);
+    setList(newData);
+  }
+
+  function editHandler(id) {
+    setEditItemId(id);
+    const data = lists.find((list) => list.id === id);
+    setEditChange(data.text);
+  }
+
+  function editchangeHandler(e) {
+    setEditChange(e.target.value);
+  }
+
+  function saveHandler(id) {
+    const updatedList = lists.map((item) =>
+      item.id === id ? { ...item, text: editChange } : item
+    );
+    setList(updatedList);
+    setEditItemId(null);
+  }
+  
 
   return (
-    <div className="App">
-    <Header />
-    <Form/>
-    <Tasks/>
-    
+    <div className='bg-black'>
+      <header className='bg-black'>
+        <h1>To-Do List</h1>
+      </header>
+      <form action="" onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="task"
+          value={input}
+          onChange={changeHandler}
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {lists.map((list) => (
+          <li key={list.id}>
+            {editItemId === list.id ? (
+              <div>
+                <input
+                  type="text"
+                  name="task"
+                  value={editChange}
+                  onChange={editchangeHandler}
+                />
+                <button onClick={() => saveHandler(list.id)}>Save</button>
+              </div>
+            ) : (
+              <div>
+                {list.text}
+                <button onClick={() => DeleteHandler(list.id)}>
+                  Delete
+                </button>
+                <button onClick={() => editHandler(list.id)}>Edit</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
