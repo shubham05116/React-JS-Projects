ec4ae86a73146ce9756078f7b9e0c2bd66b62e90
Shubham05116 <shubham05116@gmail.com>
2023-09-23 18:52:56 +0530

    adding post

--- my-app/src/Post/Posts.js
+++ my-app/src/Post/Posts.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "../Post/Posts.css"
+import Post from './Post'
 
 const Posts = () => {
   return (
     <div className='posts'>
-
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
     </div>
   )
 }
