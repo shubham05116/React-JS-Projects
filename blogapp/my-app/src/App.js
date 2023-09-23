428b7d9e9fcabb2c22b0fa94149b449449bb877d
Shubham05116 <shubham05116@gmail.com>
2023-09-23 18:24:22 +0530

    context aPi

---
+++ blogapp/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Topbar from './topbar/Topbar';
+import Home from './Pages/Home';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+  <Topbar/>
+  <Home/>
+    </div>
+  );
+}
+
+export default App;
